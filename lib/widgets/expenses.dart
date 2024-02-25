714015af7e0e18825c4050b6162512014df85de4
Neeraj Singh Sarathe <neerajssarathe@gmail.com>
2024-02-25 11:16:16 +0530

    Created Modal and basic functionality for add expense.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import '../models/expense.dart';
@@ -18,9 +19,17 @@ class _ExpensesState extends State<Expenses> {
     Expense(title: 'Cinema', amount: 69.99, date: DateTime.now(), category: Category.leisure)
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))],
+      ),
       body: Column(
         children: [Expanded(child: ExpensesList(expenses: _registeredExpenses))],
       ),
