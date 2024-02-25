e5b4e76e8e81c9535dd90ad414a6341ceb3dfa22
Neeraj Singh Sarathe <neerajssarathe@gmail.com>
2024-02-25 09:10:55 +0530

    Added expenses_list.dart
    - applied concept => ListView

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
+
+import '../models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -7,17 +10,19 @@ class Expenses extends StatefulWidget {
   State<Expenses> createState() {
     return _ExpensesState();
   }
-
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Flutter Course', amount: 19.99, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amount: 69.99, date: DateTime.now(), category: Category.leisure)
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: [
-          Text('data')
-        ],
+        children: [Expanded(child: ExpensesList(expenses: _registeredExpenses))],
       ),
     );
   }
