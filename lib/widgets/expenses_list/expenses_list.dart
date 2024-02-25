d77b37ddf5c77d3d09a356a7e2f8e5b8f9bbe2af
Neeraj Singh Sarathe <neerajssarathe@gmail.com>
2024-02-25 09:43:15 +0530

    Added Expense_item widget for styling as card and design.

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,13 +1,15 @@
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/expense.dart';
 
-class ExpensesList extends StatelessWidget{
+class ExpensesList extends StatelessWidget {
   const ExpensesList({super.key, required this.expenses});
   final List<Expense>expenses;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: expenses.length ,itemBuilder: (ctx, index) => Text(expenses[index].title));
+    return ListView.builder(
+        itemCount: expenses.length, itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]));
   }
 }
